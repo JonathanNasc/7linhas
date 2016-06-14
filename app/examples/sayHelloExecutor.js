ab2fbe62d7ad1d82c72490137ed04cde052af041
jonathan.nascimento <jonathan.nascimento@objective.com.br>
2016-06-14 19:17:14 -0300

    clean

--- app/examples/sayHelloExecutor.js
+++
@@ -1,8 +0,0 @@
-var scraper = require(APP_PATH + 'core/scraper')
-module.exports.action = 'say-hello'
-module.exports.execute = function () {
-  return scraper.test()
-    .then(function (line) {
-      return line
-    })
-}
